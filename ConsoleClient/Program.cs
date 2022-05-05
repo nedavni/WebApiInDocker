e640cf0df7dba533b23e2df5a395ee838315f41c
Nedavnii Viacheslav <vny@tobii.com>
2022-05-05 20:31:22 +0100

    Initial (NF1 violation)

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DatabaseLayer.Data;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseContext.SeedData();
+            Console.WriteLine("Hello Wljklj,orld!");
+        }
+    }
+}
---
+++ DatabaseLayer/Data/DatabaseContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DatabaseLayer.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+namespace DatabaseLayer.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        private static string ConnectionString =
+            $@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=CustomerDB;AttachDBFilename={Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "CustomerDB.mdf")}";
+
+        public DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // ?????
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseSqlServer(CreationConnectionString);
+            //}
+            
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public static void SeedData()
+        {
+            using var context = new DatabaseContext();
+            context.Database.Migrate();
+            if (!context.Customer.Any())
+            {
+                context.AddRange(Enumerable.Range(1, 50).Select(i => new Customer { Name = $"Petro, Piatochkin {i}" }));
+            }
+            context.SaveChanges();
+        }
+    }
+}
